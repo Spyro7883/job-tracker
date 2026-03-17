3db12cd925c0ea76fe1be626c583736529bf45de
Spyro7883 <spinu11_2003@yahoo.ro>
2026-03-17 19:38:19 +0200

    deploy works

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -6,6 +6,6 @@ const adapter = new PrismaPg({ connectionString });
 
 const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter });
+export const prisma =  globalForPrisma.prisma ??  new PrismaClient({ adapter: adapter as unknown as never });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
